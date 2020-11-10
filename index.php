664f741ecc950b47a39387ab48f9123d6e6b91d7
anonzakok <55388823+anonzakok@users.noreply.github.com>
2020-11-10 14:56:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+    <title>HOME</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+
+
+</body>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+
+
+   <body>
+    
+    
+   
+
+
+<br><br>
+    <u><h1 align = 'center'>สื่อประชาสัมพันธ์บ้านสวนผักยิ้มด้วยเทคโนโลยี AR</h1></u>
+
+<br>
+
+   
+&nbsp;&nbsp;&nbsp;<iframe width="490" height="350" src="https://www.youtube.com/embed/FNzGuBkLf5A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    &nbsp;&nbsp;&nbsp;<iframe width="490" height="350" src="https://www.youtube.com/embed/oTtTNgid7wM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+        &nbsp;&nbsp;&nbsp;<iframe width="490" height="350" src="https://www.youtube.com/embed/7d8oD_WOxbE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+<br><br>
+
+            <h3 align = 'center'>ชื่อนายจักรกฤษณ์ ทองโอภาส 360408241001 ITS36041N</h3><br>
+
+            <h3 align = 'center'>ชื่อนางสาวจิรดา พันธุ์สถิตย์ 360408241002 ITS36041N</h3>
+
+
+
+    </body>
+
+</html>
